d49dac3e397c1e1fd32622a2d01c87c42c39290b
Shane Rogers <rogerssh@tcd.ie>
2014-12-16 23:34:58 +0000

    share intent is failing

--- app/src/main/java/com/example/shanerogers/heywash/LoginController.java
+++ app/src/main/java/com/example/shanerogers/heywash/LoginController.java
@@ -1,27 +1,35 @@
 package com.example.shanerogers.heywash;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
-public class LoginController extends ActionBarActivity implements View.OnClickListener {
-    // UI Elements
+public class LoginController extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
+    // UI Vars
     TextView loginTextView ;
     Button loginButton ;
     ListView loginListView ;
     ArrayAdapter loginArrayAdapter;
     ArrayList valuePropList = new ArrayList();
     EditText emailInput;
+//   m prefix for namespacing the controller? m for main
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +48,8 @@ public class LoginController extends ActionBarActivity implements View.OnClickLi
         loginButton.setText("Sign In");
 
         loginListView = (ListView) findViewById(R.id.login_listView);
-        loginArrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                valuePropList);
+        loginListView.setOnItemClickListener(this);
+        loginArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, valuePropList);
 
         loginListView.setAdapter(loginArrayAdapter);
     }
@@ -50,24 +57,37 @@ public class LoginController extends ActionBarActivity implements View.OnClickLi
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login_controller, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider) shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void setShareIntent() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // create an Intent with the contents of the TextView
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Android Development");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Extra Text");
 
-        return super.onOptionsItemSelected(item);
+        // Make sure the provider knows
+        // it should work with that Intent
+        mShareActionProvider.setShareIntent(shareIntent);
     }
 
     @Override
@@ -75,5 +95,12 @@ public class LoginController extends ActionBarActivity implements View.OnClickLi
         emailInput = (EditText) findViewById(R.id.login_emailInput);
         valuePropList.add(emailInput.getText().toString());
         loginArrayAdapter.notifyDataSetChanged();
+
+        setShareIntent();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("List Item Tapped: ", position + ": " + valuePropList.get(position));
     }
 }
